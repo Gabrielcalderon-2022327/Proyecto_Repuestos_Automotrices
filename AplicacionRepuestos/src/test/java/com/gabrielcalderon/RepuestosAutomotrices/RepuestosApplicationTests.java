afaa7c1139b25daba6f19f122891fdea7ae745b5
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-03 17:50:45 -0600

    refactor: Se cambio el nombre de la aplicacion

--- AplicacionRepuestos/src/test/java/com/gabrielcalderon/RepuestosAutomotrices/RepuestosApplicationTests.java
+++ AplicacionRepuestos/src/test/java/com/gabrielcalderon/RepuestosAutomotrices/RepuestosApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Ejemplo;
+package com.gabrielcalderon.RepuestosAutomotrices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EjemploApplicationTests {
+class RepuestosApplicationTests {
 
 	@Test
 	void contextLoads() {
