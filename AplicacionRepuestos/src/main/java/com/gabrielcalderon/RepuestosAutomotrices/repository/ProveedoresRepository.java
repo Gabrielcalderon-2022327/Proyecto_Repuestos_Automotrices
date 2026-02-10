8b7ce3ba11732f2c172c515ebca9f156d7b0ca7c
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-10 08:16:51 -0600

    feat: Se agrego el repositorio para Proveedores

---
+++ AplicacionRepuestos/src/main/java/com/gabrielcalderon/RepuestosAutomotrices/repository/ProveedoresRepository.java
@@ -0,0 +1,7 @@
+package com.gabrielcalderon.RepuestosAutomotrices.repository;
+
+import com.gabrielcalderon.RepuestosAutomotrices.entity.Proveedores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedoresRepository extends JpaRepository<Proveedores, Integer> {
+}
