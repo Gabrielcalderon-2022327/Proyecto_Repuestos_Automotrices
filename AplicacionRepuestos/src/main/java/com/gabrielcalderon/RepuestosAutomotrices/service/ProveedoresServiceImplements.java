6b8051f429b7cf410315199da56cce71fd652c6c
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-10 08:39:34 -0600

    feat: Se agrego la implementación del servicio de Proveedores

---
+++ AplicacionRepuestos/src/main/java/com/gabrielcalderon/RepuestosAutomotrices/service/ProveedoresServiceImplements.java
@@ -0,0 +1,57 @@
+package com.gabrielcalderon.RepuestosAutomotrices.service;
+
+import com.gabrielcalderon.RepuestosAutomotrices.entity.Proveedores;
+import com.gabrielcalderon.RepuestosAutomotrices.repository.ProveedoresRepository;
+
+import java.util.List;
+
+public class ProveedoresServiceImplements implements ProveedoresService{
+    private final ProveedoresRepository proveedoresRepository;
+
+    public ProveedoresServiceImplements(ProveedoresRepository proveedoresRepository) {
+        this.proveedoresRepository = proveedoresRepository;
+    }
+
+    @Override
+    public List<Proveedores> getAllProveedores() {
+        return proveedoresRepository.findAll();
+    }
+
+    @Override
+    public Proveedores saveProveedor(Proveedores proveedor) throws RuntimeException {
+        return proveedoresRepository.save(proveedor);
+    }
+
+    @Override
+    public Proveedores getProveedorById(Integer id) {
+        Proveedores proveedor = proveedoresRepository.findById(id).orElse(null);
+        if (proveedor == null){
+            throw new IllegalArgumentException("No se encontro el proveedor");
+        }
+        return proveedoresRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Proveedores updateProveedores(Integer id, Proveedores proveedor) {
+        Proveedores proveedor1 = proveedoresRepository.findById(id).orElse(null);
+        if (proveedor1 != null){
+            proveedor1.setNombre_proveedor(proveedor.getNombre_proveedor());
+            proveedor1.setDireccion(proveedor.getEmail_proveedor());
+            proveedor1.setTelefono_proveedor(proveedor.getTelefono_proveedor());
+            proveedor1.setEmail_proveedor(proveedor.getEmail_proveedor());
+
+            return proveedoresRepository.save(proveedor1);
+        } else {
+            throw new IllegalArgumentException("No se encontro el proveedor");
+        }
+    }
+
+    @Override
+    public void deleteProveedor(Integer id) {
+        Proveedores proveedor = proveedoresRepository.findById(id).orElse(null);
+        if (proveedor == null){
+            throw new IllegalArgumentException("No se encontro el proveedor");
+        }
+        proveedoresRepository.deleteById(id);
+    }
+}
