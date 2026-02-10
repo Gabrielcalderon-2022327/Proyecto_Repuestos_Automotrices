ba8ed3e96e2e9536ec80374f46c108e58a7eafb8
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-10 11:41:22 -0600

    feat: Se creo la implementacion del servicio de Ventas

---
+++ AplicacionRepuestos/src/main/java/com/gabrielcalderon/RepuestosAutomotrices/service/VentasServiceImplements.java
@@ -0,0 +1,57 @@
+package com.gabrielcalderon.RepuestosAutomotrices.service;
+
+import com.gabrielcalderon.RepuestosAutomotrices.entity.Venta;
+import com.gabrielcalderon.RepuestosAutomotrices.repository.VentasRepository;
+
+import java.util.List;
+
+public class VentasServiceImplements implements VentaService{
+    private final VentasRepository repository;
+
+    public VentasServiceImplements(VentasRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Venta> getAllVentas() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Venta getVentaById(Integer id) {
+        Venta venta = repository.findById(id).orElse(null);
+        if (venta == null){
+            throw new IllegalArgumentException("No se encontro la venta");
+        }
+        return venta;
+    }
+
+    @Override
+    public Venta saveVenta(Venta venta) {
+        return repository.save(venta);
+    }
+
+    @Override
+    public Venta updateVenta(Integer id, Venta venta) {
+        Venta venta1 = repository.findById(id).orElse(null);
+        if (venta1 == null){
+            throw new IllegalArgumentException("No se encontro la venta");
+        } else {
+            venta1.setFecha_venta(venta.getFecha_venta());
+            venta1.setCantidad(venta.getCantidad());
+            venta1.setId_empleado(venta.getId_empleado());
+            venta1.setTotal(venta.getTotal());
+            venta.setId_repuesto(venta.getId_repuesto());
+        }
+        return venta1;
+    }
+
+    @Override
+    public void deleteVenta(Integer id) {
+        Venta venta = repository.findById(id).orElse(null);
+        if (venta == null){
+            throw new IllegalArgumentException("No se encontro la venta");
+        }
+        repository.deleteById(id);
+    }
+}
